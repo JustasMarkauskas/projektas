8c19194655a51f4679ad5095a74cdd83ba70b5d0
JustasMarkauskas <43873829+JustasMarkauskas@users.noreply.github.com>
2018-10-12 16:50:02 +0300

    10-12

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,1 +1,13 @@
-console.log("Pavyko!");
+// navigacinio meniu paslepimas scrolinant zemyn 
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("top-nav").style.top = "0";
+  } else {
+    document.getElementById("top-nav").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
